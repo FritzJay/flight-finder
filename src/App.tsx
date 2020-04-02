ce2351b8b613dcd3b9c56553e7ec3f3044e47e4c
Fritz Jay <jonathan.jay10@gmail.com>
2020-04-02 11:51:51 -0700

    Added redux dispatch to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -11,9 +11,10 @@ import StepLabel from "@material-ui/core/StepLabel";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "@material-ui/core";
-import { store } from "./Redux/index";
+import { RootState } from "./Redux/index";
 import Flights from "./Components/Flights/Flights";
 import Information from "./Components/Information/Information";
+import { setStep } from "./Redux/system";
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -52,6 +53,16 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const useApp = () => {
+  const dispatch = useDispatch();
+  const step = useSelector((state: RootState) => state.system.step);
+  return {
+    step,
+    handleNext: () => dispatch(setStep(step + 1)),
+    handleBack: () => dispatch(setStep(step - 1))
+  };
+};
+
 const steps = ["Information", "Flights", "Lodging", "Car Rentals"];
 
 const getStepContent = (step: number) => {
@@ -71,82 +82,72 @@ const getStepContent = (step: number) => {
 
 const App = () => {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep(activeStep - 1);
-  };
+  const { step, handleBack, handleNext } = useApp();
 
   return (
     <React.Fragment>
-      <Provider store={store}>
-        <CssBaseline />
-        <AppBar position="absolute" color="default" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              Flight Finder
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <main className={classes.layout}>
-          <Paper className={classes.paper}>
-            <Typography component="h1" variant="h4" align="center">
-              Book Flights
-            </Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
-              {steps.map(label => (
-                <Step key={label}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            <React.Fragment>
-              {activeStep === steps.length ? (
-                <React.Fragment>
-                  <Typography variant="h5" gutterBottom>
-                    Thank you for your order.
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    Your order number is #2001539. We have emailed your order
-                    confirmation, and will send you an update when your order
-                    has shipped.
-                  </Typography>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  {getStepContent(activeStep)}
-                  <div className={classes.buttons}>
-                    {activeStep !== 0 && (
-                      <Button onClick={handleBack} className={classes.button}>
-                        Back
-                      </Button>
-                    )}
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={handleNext}
-                      className={classes.button}
-                    >
-                      {activeStep === steps.length - 1 ? "Place order" : "Next"}
-                    </Button>
-                  </div>
-                </React.Fragment>
-              )}
-            </React.Fragment>
-          </Paper>
-
-          <Typography variant="body2" color="textSecondary" align="center">
-            {"Powered By "}
-            <Link color="inherit" href="https://www.certify.com/">
-              Certify Travel
-            </Link>
+      <CssBaseline />
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Flight Finder
           </Typography>
-        </main>
-      </Provider>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            Book Flights
+          </Typography>
+          <Stepper activeStep={step} className={classes.stepper}>
+            {steps.map(label => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            {step === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
+                </Typography>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(step)}
+                <div className={classes.buttons}>
+                  {step !== 0 && (
+                    <Button onClick={handleBack} className={classes.button}>
+                      Back
+                    </Button>
+                  )}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {step === steps.length - 1 ? "Place order" : "Next"}
+                  </Button>
+                </div>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        </Paper>
+
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Powered By "}
+          <Link color="inherit" href="https://www.certify.com/">
+            Certify Travel
+          </Link>
+        </Typography>
+      </main>
     </React.Fragment>
   );
 };
