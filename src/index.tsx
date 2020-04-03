90c69377285840ddabe35becb7d76a217aac7bde
Fritz Jay <jonathan.jay10@gmail.com>
2020-04-03 16:54:06 -0700

    Removed unused files. Updated architecture.png. Added a custom favicon. Updated manifest.json. Updated README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { store } from "./Redux";
 
 ReactDOM.render(
@@ -14,8 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
